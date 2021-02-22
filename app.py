b05ddbb2a3c6ad734416e04b390b5bec0fc2f671
Corsicaa <59146570+Corsicaa@users.noreply.github.com>
2021-02-22 16:41:18 +0100

    Push du projet

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+import json
 
 app = Flask(__name__) 
 
+app.config['MONGO_URI'] = "mongodb+srv://admin:admin@ynovbdd.ayg81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
 @app.route("/") 
 def home(): 
-    return render_template('pages/page.html')
+    varMatch = {}
+    varProject = {}
+    varSort = {}
+    all = mongo.db.Photos.aggregate([varMatch, varProject, varSort])
+    resp = dumps(all)
+    jsonData = json.loads(resp)
+    return render_template('pages/page.html', jsonData = jsonData)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -12,4 +25,5 @@ def login():
         return render_template('pages/login.html')
     if "submit" in request.form:
         return redirect('/')
+
 app.run(debug = True)
