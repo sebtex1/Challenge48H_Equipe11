97a6375a0d8f6d9659f506b27fb2d03ed198278b
SebTex <36498890+sebtex1@users.noreply.github.com>
2021-02-22 23:46:25 +0100

    recherche

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
 import json
+import urllib.parse
 
 app = Flask(__name__) 
 
@@ -12,20 +13,33 @@ mongo = PyMongo(app)
 if __name__ == "__main__":
     app.run(debug=False)
 
-@app.route("/") 
-def home(): 
-    varMatch = { "$match": {"$and": [{"tags": "Produit"}]} }
-    varProject = { "$project": {"nom": 1, "chemin": 1, "tags": 1, "_id": 0}}
-    # varSort = {}
-    items = mongo.db.Photos.aggregate([varProject])
-    resp = dumps(items)
-    jsonData = json.loads(resp)
-    return render_template('pages/page.html', jsonData = jsonData)
+@app.route("/photos", methods=["GET", "POST"])
+def home():
+    matchList = {}
+    varMatch = {}
+    if request.args:
+        if request.args.get('nom'):
+            nom = request.args.get('nom')
+            matchList['nom']={'$regex':nom}
 
-@app.route("/login", methods=['GET', 'POST'])
+        if request.args.get('tags'):
+            tags = request.args.get('tags')
+            matchList['tags']={"$and": [{"tags": tags}]}
+
+        varMatch['$match']=matchList
+        items = mongo.db.Photos.aggregate([varMatch])
+        resp = dumps(items)
+        jsonData = json.loads(resp)
+        return render_template('pages/page.html', jsonData = jsonData)
+
+    if request.method == 'GET':
+        message = "Executez donc une recherche de photos"
+        return render_template('pages/page.html', message=message)
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'GET':
         return render_template('pages/login.html')
     if "submit" in request.form:
-        return redirect('/')
+        return redirect('/photos')
 
