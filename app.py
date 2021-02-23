f89ea2a57f8684ccd449373090bf4bb7bf125494
Corsicaa <59146570+Corsicaa@users.noreply.github.com>
2021-02-23 09:48:15 +0100

    add info.html

--- app.py
+++ app.py
@@ -51,3 +51,6 @@ def login():
     if "submit" in request.form:
         return redirect('/photos')
 
+@app.route("/info", methods=['GET', 'POST'])
+def info():
+    return render_template('pages/info.html')
